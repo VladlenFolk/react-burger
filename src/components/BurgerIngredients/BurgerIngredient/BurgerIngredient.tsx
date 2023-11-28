04dd1d6cc361d8add6a602fba84f72892fe09eef
Avok@to34 <vladlen-17@yandex.ru>
2023-11-28 13:50:34 +0300

    feat: сделал два компонента с ингридиентом

--- src/components/BurgerIngredients/BurgerIngredient/BurgerIngredient.tsx
+++ src/components/BurgerIngredients/BurgerIngredient/BurgerIngredient.tsx
@@ -1,15 +1,8 @@
-import {
-  CurrencyIcon,
-  Counter,
-} from "@ya.praktikum/react-developer-burger-ui-components";
-import { nanoid } from "nanoid";
 import { useAppSelector } from "../../../hooks/typesHooks";
 import burgerStyle from "./BurgerIngredient.module.css";
-import { useDrag } from "react-dnd/dist/hooks";
-import { Link, useLocation } from "react-router-dom";
 import { TIngredient } from "../../../types/types";
-import { addBun, addOtherIngredient, countOpen } from "../../../services/reduxToolkit/constructorSlice";
-import { useAppDispatch } from "../../../hooks/typesHooks";
+import BurgerIngredientSmall from "./BurgerIngredientSmall/BurgerIngredientSmall";
+import BurgerIngredientLarge from "./BurgerIngredientLarge/BurgerIngredientLarge";
 
 type TIngredientBurger = {
   count: number;
@@ -18,67 +11,11 @@ type TIngredientBurger = {
 };
 
 const BurgerIngredient: React.FC<TIngredientBurger> = ({ count, item, id }) => {
-  const location = useLocation();
-  const [{ opacity }, dragRef] = useDrag({
-    type: "ingredients",
-    item,
-    collect: (monitor) => ({
-      opacity: monitor.isDragging() ? 0.5 : 1,
-    }),
-  });
-
   const { windowSize } = useAppSelector((state) => state.windowSlice);
- 
-  let src = windowSize > 900 ? item.image : item.image_mobile;
-  const dispatch = useAppDispatch();
- //Функция добавления перемещенного элемента
- const addItem = (item: TIngredient) => {
-  const ingredient = {
-    item,
-    id: nanoid(),
-  };
-  if (item.type === "bun") {
-    dispatch(addBun(item));
-    dispatch(countOpen())
-  } else {
-    dispatch(addOtherIngredient(ingredient));
-    dispatch(countOpen())
-  }
-};
-
-
   return (
     <div className={burgerStyle.container}>
       <div className={burgerStyle.cover}></div>
-      <Link
-        to={{ pathname: `/ingredient/${id}`, state: { background: location } }}
-        className={burgerStyle.ingredientsComponent}
-        style={{ opacity }}
-        draggable={true}
-        ref={dragRef}
-      >
-        <div className={burgerStyle.ingredientsComponent}>
-          {count > 0 && <Counter count={count} size="default" />}
-          <img className={burgerStyle.image} src={src} alt={item.name}></img>
-          <div className={burgerStyle.price}>
-            <p className="text text_type_digits-default mr-2"> {item.price}</p>
-            <CurrencyIcon type="primary" />
-          </div>
-          <p className="text text_type_main-default mt-2">{item.name}</p>
-        </div>
-      </Link>
-      <div className={burgerStyle.ingredientsComponentMin} onClick={()=>addItem(item)}>
-        {count > 0 && <Counter count={count} size="small" />}
-        <img className={burgerStyle.image} src={src} alt={item.name}></img>
-        <div className={burgerStyle.price}>
-          <p className="text text_type_digits-default mr-2"> {item.price}</p>
-          <CurrencyIcon type="primary" />
-        </div>
-        <p className="text text_type_main-default mt-2">{item.name}</p>
-        <p className={`${"text text_type_secondary-default"} ${burgerStyle.add}`}>
-        {"Добавить"}
-        </p>
-      </div>
+      {windowSize > 1060 ? <BurgerIngredientLarge count={count} item={item} id={id}/> : <BurgerIngredientSmall count={count} item={item} id={id}/>}
     </div>
   );
 };
