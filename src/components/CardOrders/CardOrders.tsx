52999a4aea561c51d1c7e77aea0ffa5f98e053c2
Avok@to34 <vladlen-17@yandex.ru>
2023-12-10 09:55:30 +0300

    feat: сделал элемент кард общим для истории заказов и ленты

--- src/components/CardOrders/CardOrders.tsx
+++ src/components/CardOrders/CardOrders.tsx
@@ -4,17 +4,19 @@ import {
   FormattedDate,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Link, useLocation } from "react-router-dom";
-import { useAppSelector } from "../../../hooks/typesHooks";
-import { getPrice, getObjWithCount } from "../../../utils/functions";
+import { useAppSelector } from "../../hooks/typesHooks";
+import { getPrice, getObjWithCount } from "../../utils/functions";
 import { useMemo } from "react";
-import { TOrders } from "../../../types/types";
+import { TOrders } from "../../types/types";
 
 type TCardProps = {
-  order:  TOrders
-}
+  order: TOrders;
+};
 
-const CardOrders: React.FC<TCardProps> =({ order }) => {
+const CardOrders: React.FC<TCardProps> = ({ order }) => {
   const location = useLocation();
+  console.log(location);
+
   const ingredientsArr = order.ingredients;
   const date = order.createdAt;
   const { ingredients } = useAppSelector((state) => state.ingredientsSlice);
@@ -35,6 +37,11 @@ const CardOrders: React.FC<TCardProps> =({ order }) => {
     : (count = 0);
   const number = order.number;
   const status = order.status === "done" ? "Готов" : "Создан";
+  const statusStile =
+    location.pathname === "/feed"
+      ? styleCard.status
+      : `${"text text_type_main-default mt-2 ml-1"}`;
+  console.log(statusStile);
 
   return (
     <Link
@@ -47,12 +54,22 @@ const CardOrders: React.FC<TCardProps> =({ order }) => {
       <li className={styleCard.card}>
         <div className={styleCard.cardHeader}>
           <p className="text text_type_digits-default">{`#${order.number}`}</p>
-          <p className={`${"text text_type_main-default text_color_inactive"} ${styleCard.date}`}>
-            {<FormattedDate date={new Date(date)} />}&nbsp;{'i-GMT+3'}
+          <p
+            className={`${"text text_type_main-default text_color_inactive"} ${
+              styleCard.date
+            }`}
+          >
+            {<FormattedDate date={new Date(date)} />}&nbsp;{"i-GMT+3"}
           </p>
         </div>
-        <h3 className={`${"text text_type_main-medium mt-6 ml-1"} ${styleCard.orderName}`}>{order.name}</h3>
-        <p className="text text_type_main-default mt-2 ml-1">{status}</p>
+        <h3
+          className={`${"text text_type_main-medium mt-6 ml-1"} ${
+            styleCard.orderName
+          }`}
+        >
+          {order.name}
+        </h3>
+        <p className={statusStile}>{status}</p>
         <div className={styleCard.mainContainer}>
           <ul className={styleCard.list}>
             {wisibleIngredients.map((ingredient, index) => (
@@ -75,13 +92,19 @@ const CardOrders: React.FC<TCardProps> =({ order }) => {
             ))}
           </ul>
           <div className={styleCard.total}>
-            <p className={`${"text text_type_digits-default"} ${styleCard.digits}`}>{price}</p>
+            <p
+              className={`${"text text_type_digits-default"} ${
+                styleCard.digits
+              }`}
+            >
+              {price}
+            </p>
             <CurrencyIcon type="primary" />
           </div>
         </div>
       </li>
     </Link>
   );
-}
+};
 
 export default CardOrders;
--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,17 +1,29 @@
 import styleFeed from "./Feed.module.css";
 import CardFeed from "./Card/CardFeed";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppSelector, useAppDispatch } from "../../hooks/typesHooks";
 import {
   wsConnectionStart,
   wsClosed,
 } from "../../services/reduxToolkit/webSocketSlice";
+import CardOrders from "../../components/CardOrders/CardOrders";
 
 function Feed() {
+  const [tab, setTab] = useState("1");
+  const handleOrdersClick = () => {
+    setTab("1");
+  };
+  const handleStatisticClick = () => {
+    setTab("2");
+  };
   const dispatch = useAppDispatch();
-  const { orders, total, totalToday } = useAppSelector((state) => state.wsSlice);
+  const { orders, total, totalToday } = useAppSelector(
+    (state) => state.wsSlice
+  );
 
   const feedUrl = "wss://norma.nomoreparties.space/orders/all";
+  const tabOrders = useRef<HTMLDivElement>(null);
+  const tabStatistic = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     dispatch(wsConnectionStart(feedUrl));
@@ -20,9 +32,36 @@ function Feed() {
     };
   }, []);
 
+  const bunTab =
+    tab === "1" ? styleFeed.tab_menu_type_current : styleFeed.noselected;
+  const sauceTab =
+    tab === "2" ? styleFeed.tab_menu_type_current : styleFeed.noselected;
+
   return (
     <section className={styleFeed.feed}>
       <div className={styleFeed.container}>
+      <div className={styleFeed.titleMobile}>
+        <h2 className="text text_type_main-large ml-20">Лента заказов</h2>
+      </div>
+        <div className={styleFeed.title}>
+          <h2 className="text text_type_main-large ml-20">Лента заказов</h2>
+        </div>
+        <div id="menu" className={`${styleFeed.tabs} ${bunTab}`}>
+          <div
+            className={`${styleFeed.tab_menu} ${bunTab}`}
+            onClick={handleOrdersClick}
+            ref={tabOrders}
+          >
+            <span className="text text_type_main-default">Заказы</span>
+          </div>
+          <div
+            className={`${styleFeed.tab_menu} ${sauceTab}`}
+            onClick={handleStatisticClick}
+            ref={tabStatistic}
+          >
+            <span className="text text_type_main-default">Статистика</span>
+          </div>
+        </div>
         <div className={styleFeed.scrollContainer}>
           <div className={styleFeed.title}>
             <h2 className="text text_type_main-large ml-20">Лента заказов</h2>
@@ -30,7 +69,7 @@ function Feed() {
           <div className={styleFeed.scroll}>
             <ul className={styleFeed.cardsList}>
               {orders.map((order) => (
-                <CardFeed key={order._id} order={order} />
+                <CardOrders key={order._id} order={order} />
               ))}
             </ul>
           </div>
